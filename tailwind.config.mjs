5e766f3a6c0663b434379ed080acc05bfb3f1f8f
JorgeMorera <jorgemorera18@hotmail.com>
2024-04-13 17:26:52 -0600

    tipografias

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'cinzel': ['Cinzel Decorative', 'cursive'],
+			'creepster': ['Creepster', 'cursive'],
+			'girassol': ['Girassol', 'cursive'],
+			'nosifer': ['Nosifer', 'cursive'],
+			'unifrakturcook': ['UnifrakturCook', 'cursive'],
+			'unifrakturmaguntia': ['UnifrakturMaguntia', 'cursive'],
+		}
 	},
 	plugins: [],
 }
