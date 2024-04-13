e7246eeac43ab8c8a15d1b152c0938f9ad021d74
JorgeMorera <jorgemorera18@hotmail.com>
2024-04-12 18:41:07 -0600

    add pages,pics, ts archive

---
+++ src/data/discografia.ts
@@ -0,0 +1,33 @@
+//hacer un json con los datos de los albnumes de la discografia
+
+const discografia = [
+    
+    {
+        id: 1,
+        year: 1995,
+        artist: 'Cangrena',
+        album: 'Sad End Of Flesh',
+        img: 'https://i.discogs.com/9Tfu6WcQe1lUyhuCgOtPMkextKThCQ06ZvGVZ9O6mdU/rs:fit/g:sm/q:90/h:600/w:380/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTcwMTk5/MjktMTYzNjIwODU3/OS04OTA0LmpwZWc.jpeg',
+        songs: ['Grotesque Separation Of Extremities', 'Legacy Of Faith', 'The Crucifix', 'Prenatal Operating Room','Voodoo Death', 'Infecto', 'Face Of Hipocresy'],
+    },
+    {
+        id: 2,
+        year: 1999,
+        artist: 'Cangrena',
+        album: 'Demoniak Transition',
+        img: 'https://i.discogs.com/Zgp-pxUzzw03zy_bxMeb_svQHpIN65DfO4CjDK5etoE/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTkyMTg0/OS0xMzQ3Nzc4MTEx/LTU0MjMuanBlZw.jpeg',
+        songs: ['My Kingdom Is Under The Shadows', 'Unnatural Penetration', 'Partido A La Mitad', 'Return To Pain','Possession Completee','	Dark Harresment','Predessesor (Intro)',' When The Putrefaction Arrives To Us','Abysmal'],
+    },
+    {
+        id: 3,
+        year: 2014,
+        artist: 'Cangrena',
+        album: 'En Carne Viva',
+        img: 'https://i.discogs.com/RtrTyf3HJt4_3uC5LvfDm0BosbvBaR6eft-O0ipIF1A/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwMzMy/MjE2LTE2MzIzNTIx/NjYtNTE1MC5qcGVn.jpeg',
+        songs: ['Puesti-Tica', 'Beso Negro', 'Pisuicas', 'Pesadilla En Carne Viva','Si, Yo La Maté','Triángulo De La Muerte (1983-1996)','En Carne Propia (Las 3 Malditas Personas)','Partido A La Mitad (2004)'],
+    },
+
+]
+
+export default discografia
+
